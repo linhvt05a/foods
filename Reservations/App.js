8cfe23b18fd37f434fa3d528bfc2befc59a8306d
iMac <hoanglinh6794@gmail.com>
2021-06-22 08:43:17 +0700

    add navigation

--- Reservations/App.js
+++ Reservations/App.js
@@ -1,9 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { View, Text } from 'react-native'
 import {HomeScreen} from './src/screens'
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      
+    </NavigationContainer>
   )
 }
